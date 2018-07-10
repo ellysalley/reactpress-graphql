71bc3d0ecca94d8654e43169211fa6991d1e7719
yann <yann.boisselier@gmail.com>
2018-07-10 11:43:57 +0200

    begin authentication

---
+++ themes/starter/pages/login.js
@@ -0,0 +1,10 @@
+import React from "react";
+import LoginForm from "../components/ui/LoginForm";
+
+class LoginPage extends React.Component {
+  render() {
+    return <LoginForm />;
+  }
+}
+
+export default LoginPage;
