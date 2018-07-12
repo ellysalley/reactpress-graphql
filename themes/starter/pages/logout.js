09fb04ec55bd816e1060ad95b2444ef7b08f6d5a
yann <yann.boisselier@gmail.com>
2018-07-12 16:09:18 +0200

    add logout page

---
+++ themes/starter/pages/logout.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { withRouter } from "next/router";
+import { compose } from "recompose";
+import withNextApollo, { apolloClient } from "../../../lib/withNextApollo";
+import { setToken } from "../../../lib/auth";
+
+class LogoutPage extends React.Component {
+  componentDidMount() {
+    setToken(null);
+    this.props.router.push("/");
+  }
+
+  render() {
+    return null;
+  }
+}
+
+export default compose(withRouter)(LogoutPage);
