f364dd3e3fb520d4855cb9bb0a3333da8c2a095d
yann <yann.boisselier@gmail.com>
2018-07-12 12:49:10 +0200

    add login page

--- themes/starter/pages/login.js
+++ themes/starter/pages/login.js
@@ -1,10 +1,69 @@
 import React from "react";
+import { withRouter } from "next/router";
 import LoginForm from "../components/ui/LoginForm";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
+import { compose } from "recompose";
+import withNextApollo from "../../../decorators/withNextApollo";
+import { setToken } from "../../../lib/auth";
 
-class LoginPage extends React.Component {
+/**
+ * Logs user to Wordpress from the LoginForm user and password
+ * @param {React.Component} LoginForm
+ */
+class LoginFormContainer extends React.Component {
+  state = {
+    graphQLErrors: null
+  };
+  handleSubmit = async ({ password, username }) => {
+    const { login, history } = this.props;
+    try {
+      const result = await login({ variables: { password, username } });
+      setToken(result.data.login.authToken);
+      // await apolloClient.resetStore();
+      //history.push("/");
+    } catch (error) {
+      this.setState({
+        errors: error.graphQLErrors
+      });
+    }
+  };
+
+  componentDidMount() {
+    //this.timeout = setInterval(this.props.data.refetch, 1000);
+  }
+
+  componentWillUnmount() {
+    //clearInterval(this.timeout);
+  }
+
   render() {
-    return <LoginForm />;
+    return (
+      <div>
+        {this.state.errors && JSON.stringify(this.state.errors)}
+        <LoginForm onSubmit={this.handleSubmit} />
+      </div>
+    );
   }
 }
 
-export default LoginPage;
+const query = gql`
+  mutation logToWordpress($username: String!, $password: String!) {
+    login(
+      input: {
+        username: $username
+        password: $password
+        clientMutationId: "42"
+      }
+    ) {
+      authToken
+      refreshToken
+    }
+  }
+`;
+
+export default compose(
+  withRouter,
+  withNextApollo,
+  graphql(query, { name: "login" })
+)(LoginFormContainer);
